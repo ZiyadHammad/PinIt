68a07b3bc07d10e79cdfdc24006f24632bb81244
Ziyad Hammad <ziyad.hammad@outlook.com>
2022-05-03 20:24:20 -0400

    Starting react app

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-
+const userRoute = require("./routes/users")
+const pinRoute = require("./routes/pins")
 //creating express app
 const app = express()
 
@@ -11,16 +12,18 @@ dotenv.config()
 app.use(express.json())
 
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
-  useNewUrlParser: true
-})
+  useNewUrlParser: true,
+}) 
   .then(() => {
   console.log("MONGODB CONNECTED")
   })
   .catch(err => console.log(err))
 
-  
+app.use("/api/pins", pinRoute)
+app.use("/api/users", userRoute)
 
 app.listen(8801, () => {
   console.log("BACKEND SERVER IS RUNNING!")
--- api/models/User.js
+++ api/models/User.js
@@ -16,11 +16,11 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true,
+    require: true,
     min: 6,
   }
 },
   {timestamps: true}
 ) 
 
-module.export = mongoose.model("User", UserSchema)
+module.exports = mongoose.model("User", UserSchema)
