f04e618f13677f1e136a8c315c44e65f03fbab16
Ziyad Hammad <ziyad.hammad@outlook.com>
2022-05-11 15:24:27 -0400

    Working on pop up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,49 @@
+import React, {useState} from 'react';
+import Map, { Marker, Popup } from 'react-map-gl';
+import {Room} from "@material-ui/icons"
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 37.8,
+    longitude: -122.4,
+    zoom: 3,
+   
+  });
+
+  const [showPopup, togglePopup] = useState(true)
   return (
-    <div className="App">
-  Hello
-    </div>
+    <Map
+      {...viewport}
+      onMove={next => setViewport(next)}
+      style={{width: "100vw", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+    >
+      <Marker longitude={-112.4} latitude={30.8} color="red">
+        <Room style={{ fontSize: viewport.zoom * 10, color: "slateblue"}} />
+      </Marker>
+      {showPopup && (
+      <Popup longitude={-112.4} latitude={30.8}
+          anchor="left"
+          closeButton={true}
+          closeOnClick={false}
+        >
+          <div className="card">
+            <label>Location</label>
+            <h4 className="place" >Waffle House</h4>
+            <label>Review</label>
+            <p>Beautiful Place</p>
+            <label>Rating</label>
+
+            <label>Information</label>
+        </div>
+      </Popup>)}
+    </Map>
   );
 }
 
-export default App;
+export default App
